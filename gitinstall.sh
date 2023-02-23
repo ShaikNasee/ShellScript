c202b5ad87f8d0833fe1d86ff6fd7c3c9880bf11
shaik <shaiknaseer@gmail.com>
2023-02-23 20:22:40 +0530

    corrected the statement

--- gitinstall.sh
+++ gitinstall.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "git is installing "
 echo "installation is started"
-if [ $(uname) -- "Liunx"]
-then {
-    "sudo apt install git -y"
-}
+if [ "$(uname)" == "Liunx"];
+then 
+    echo "this is linux so installing git"
+    sudo apt install git -y
+
 else {
     echo "not installing git "
 }
